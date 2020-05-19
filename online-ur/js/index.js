a2f6a2d3c44f2d9ad04a9f869bded5a1586dc6e3
Frostbyte18 <37356780+Frostbyte18@users.noreply.github.com>
2020-05-19 16:57:23 -0500

    Gameboard update

---
+++ online-ur/js/index.js
@@ -0,0 +1,15 @@
+function placePiece(team, location){
+  //Team 1 or 2
+  //Location 0-13
+  newPiece = document.getElement()
+  if(location < 4){
+    //Not equal territory
+    if(team == 1){
+
+    } else if (team == 2) {
+
+    }
+  } else{
+
+  }
+}
