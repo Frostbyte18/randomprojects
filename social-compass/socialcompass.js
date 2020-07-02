884e37d8a1d084e2d7b14c7d7d878de6dfa85baa
Frostbyte18 <37356780+Frostbyte18@users.noreply.github.com>
2020-07-01 23:43:49 -0500

    Started question parsing algorithm

--- social-compass/socialcompass.js
+++ social-compass/socialcompass.js
@@ -1,10 +1,19 @@
 //Hide sample questions
 document.getElementById("question-div").style.visibility = "hidden"
 
+//Question format is ["question", [answers]]
+//[answers] format  is
 const questions = [
-  [0, ""]
+  ["", []]
 ];
 
-for(question in questions){
+questionElements = [];
 
+for(question in questions){
+  newQuestion = document.getElementById("question-template").cloneNode(true);
+  newQuestion.getElementsByClassName("question-text")[0].innerHtml = question[0];
+  answer-template = newQuestion.getElementsByClassName
+  for(answers in question[1]){
+
+  }
 }
