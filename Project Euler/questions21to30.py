07868b65bff4372b7c52f605a678d105c68808cc
Frostbyte18 <37356780+Frostbyte18@users.noreply.github.com>
2020-07-04 22:48:47 -0500

    Solved more project euler problems

--- Project Euler/questions21to30.py
+++ Project Euler/questions21to30.py
@@ -1,4 +1,5 @@
 import math
+import json
 from pprint import pprint
 
 #Question 21: Find the sum of all the amicable numbers under 10000
@@ -11,11 +12,46 @@ def question21():
         return int(d)
     s=0
     for i in range(0, 10000):
-        if i == d(d(i)) and d(i) != 1:
-            print(i, d(i))
-            s+= i
+        if i == d(d(i)):
+            if d(i) != 1 and i != d(i):
+                print(i, d(i))
+                s+= i
+    print(s)
+#Answer: 31626
+
+#Question 22: Find the total of scores of the names in names.txt
+def question22():
+    #Put names into a usable array
+    names = json.loads("[" + open("names.txt", "r").read() + "]")
+    names.sort()
+    def score(word, position):
+        d = 0
+        for letter in word:
+            d += " ABCDEFGHIJKLMNOPQRSTUVWXYZ".index(letter)
+        return d * position
+    s = 0
+    for i in range(len(names)):
+        s += score(names[i], i+1)
+    print(s)
+#Answer: 871198282
+
+#Question 23: Find the sum of all positive integers which cannot be written as the sum of two abundant numbers
+def question23():
+    def isntAbundant(n):
+        d = 1
+        for i in range(2,math.ceil(math.sqrt(n))):
+            if(n % i == 0):
+                d += i + (n/i)
+        return d <= n
+    #Generate a list of abundant numbers
+    nonabundantNumbers = []
+    for i in range(12, 28123): #28123 is largest number that's the usm of two abundant numbers
+        if isAbundant(i):
+            abundantNumbers.append(i)
+
+    
     print(s)
 
 #A shorthand way so I can quickly swap trials
-questionNumber = 21
+questionNumber = 23
 exec("question{qn}()".format(qn=questionNumber))
